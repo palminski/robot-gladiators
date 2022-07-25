8dd0ea35e5443d75710edd96d42c8f7580d109a5
palminski <williambolls98@gmail.com>
2022-07-25 16:50:48 -0700

    Added string plus name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!");
+// This creates a function
+function fight() {
+    window.alert("The Fight has Begun!");
+}
+
+var playerName = window.prompt("What is your Robot's name?");
+
+console.log("Our Robot's name is "+playerName);
+
+// fight();
