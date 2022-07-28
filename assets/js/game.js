436c50d65346882e8c7e01da182065be9f54d7bf
palminski <williambolls98@gmail.com>
2022-07-27 22:24:56 -0700

    numbers for input

--- assets/js/game.js
+++ assets/js/game.js
@@ -106,19 +106,17 @@ let endGame = function () {
 
 let shop = function () {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store?"
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth;
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert ("Leaving store");
             break;
         default:
